429b2a5fba80f08c18e2211b41ffa48c82c36abe
Dave Parfitt <diparfitt@gmail.com>
2012-02-08 16:06:10 -0500

    initial add

---
+++ lib/butter.rb
@@ -0,0 +1,57 @@
+require 'parslet'
+
+class Butter < Parslet::Parser
+	rule(:lparen)     { str('(') >> space? }
+	rule(:rparen)     { str(')') >> space? }
+	rule(:lcurly)     { str('{') >> space? }
+	rule(:rcurly)     { str('}') >> space? }
+	rule(:lbracket)   { str('[') >> space? }
+	rule(:rbracket)   { str(']') >> space? }
+	rule(:comma)      { str(',') >> space? }
+	rule(:colon)      { str(':') >> space? }
+
+	rule(:op_equals)  { str('=') >> space? }
+	
+	rule(:let)        { str("let") >> space? }
+
+	rule(:space)      { match('\s').repeat(1) }
+	rule(:space?)     { space.maybe }
+
+	# https://gist.github.com/966020
+    rule(:number) {
+      (
+        str('-').maybe >> (
+          str('0') | (match('[1-9]') >> digit.repeat)
+        ) >> (
+          str('.') >> digit.repeat(1)
+        ).maybe >> (
+          match('[eE]') >> (str('+') | str('-')).maybe >> digit.repeat(1)
+        ).maybe
+      ).as(:number)
+    }
+
+	# https://gist.github.com/966020
+    rule(:string) {
+      str('"') >> (
+        str('\\') >> any | str('"').absent? >> any
+      ).repeat.as(:string) >> str('"')
+    }
+	
+	rule(:integer)    { match('[0-9]').repeat(1).as(:int) >> space? }
+	rule(:identifier) { (match['a-z'] >> match['a-zA-Z0-9_'].repeat).as(:id) >> space? }
+	rule(:operator)   { match('[+]') >> space? }
+
+	rule(:letexpr)	  { let >> identifier >> op_equals >> expression }
+
+	rule(:lst) 			{ (lbracket >> expression >> 
+							(comma >> expression).repeat >> rbracket).as(:lst) }	
+    
+    rule(:mapentry)		{ string.as(:mapkey) >> colon >> expression.as(:mapvalue) }
+    rule(:mapentries) 	{ mapentry >> (comma >> mapentry).repeat}
+    rule(:map) 			{ lcurly >> mapentries.maybe.as(:map) >> rcurly}
+	rule(:expression) { integer | string | identifier | lst | map }
+
+	rule(:program)    { letexpr.repeat(1) }
+			
+	root :program
+end
---
+++ lib/buttertests.rb
@@ -0,0 +1,57 @@
+require '.\butter.rb'
+require "test/unit"
+
+class TestParser < Test::Unit::TestCase 
+
+	def test_identifier		
+		p = Butter.new
+		assert_equal("x", p.identifier.parse("x")[:id],"Single char ids")		
+
+		assert_equal("abc", p.identifier.parse("abc")[:id],"Multi char ids")		
+		assert_equal("a_b",p.identifier.parse("a_b")[:id],"Multi id with underscore")
+		assert_equal("aBC",p.identifier.parse("aBC")[:id],"Mutil id with mixed case")
+					
+		assert_raise(Parslet::ParseFailed) { Butter.new.identifier.parse("ABC") }		
+		assert_raise(Parslet::ParseFailed) { Butter.new.identifier.parse("_ABC") }
+		assert_raise(Parslet::ParseFailed) { Butter.new.identifier.parse("1ABC") }
+		assert_raise(Parslet::ParseFailed) { Butter.new.identifier.parse("_") }
+	end	
+
+	def test_list		
+		lst = Butter.new.lst.parse("[0,1,2]")
+		assert_not_nil(lst, "Simple integer list")
+		assert_equal(0,lst[:lst][0][:int].to_i)		
+		assert_equal(1,lst[:lst][1][:int].to_i)
+		assert_equal(2,lst[:lst][2][:int].to_i)
+				
+		nested = Butter.new.lst.parse("[1,2,[a,b,c]]")
+		assert_not_nil(nested, "Nested list")
+		assert_equal("a",nested[:lst][2][:lst][0][:id].to_s)
+		assert_equal("b",nested[:lst][2][:lst][1][:id].to_s)
+		assert_equal("c",nested[:lst][2][:lst][2][:id].to_s)
+	end
+	
+	def test_map
+		map = Butter.new.map.parse('{"Foo":1,"Bar":2}')		
+		assert_equal("Foo",map[:map][0][:mapkey][:string])
+		assert_equal(1,map[:map][0][:mapvalue][:int].to_i)
+
+		assert_equal("Bar",map[:map][1][:mapkey][:string])
+		assert_equal(2,map[:map][1][:mapvalue][:int].to_i)
+
+		assert_not_nil(Butter.new.map.parse('{"Foo":1,"Bar":[1,2,3]}'),"Map with a nested list")		
+		
+		assert_not_nil(Butter.new.map.parse('{"Foo":1,"Bar":{}}'),"Map with an empty nested map")
+	end
+
+	def test_equals
+		assert_not_nil(Butter.new.op_equals.parse("="))
+		assert_not_nil(Butter.new.op_equals.parse("= "))
+	end
+
+	def test_let		
+		Butter.new.parse("let foo = 1")
+		Butter.new.parse("let foo = [1,2,3,4]")
+		Butter.new.parse('let foo = [1,2,3,{"Foo":"Bar"}]')		
+	end	
+end
